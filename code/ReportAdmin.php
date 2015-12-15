93fced94f1d0e11ad6b7c1af8c8ea57a7adff4e8
helpfulrobot <chris@silverstripe.com>
2015-12-16 11:06:45 +1300

    Converted to PSR-2

--- code/ReportAdmin.php
+++ code/ReportAdmin.php
@@ -10,180 +10,197 @@
  * 
  * @package reports
  */
-class ReportAdmin extends LeftAndMain implements PermissionProvider {
-	
-	private static $url_segment = 'reports';
-	
-	private static $url_rule = '/$ReportClass/$Action';
-	
-	private static $menu_title = 'Reports';	
-	
-	private static $template_path = null; // defaults to (project)/templates/email
-	
-	private static $tree_class = 'SS_Report';
+class ReportAdmin extends LeftAndMain implements PermissionProvider
+{
+    
+    private static $url_segment = 'reports';
+    
+    private static $url_rule = '/$ReportClass/$Action';
+    
+    private static $menu_title = 'Reports';
+    
+    private static $template_path = null; // defaults to (project)/templates/email
 
-	private static $url_handlers = array(
-		'$ReportClass/$Action' => 'handleAction'
-	);
+    private static $tree_class = 'SS_Report';
 
-	/**
-	 * Variable that describes which report we are currently viewing based on 
-	 * the URL (gets set in init method).
-	 *
-	 * @var string
-	 */
-	protected $reportClass;
+    private static $url_handlers = array(
+        '$ReportClass/$Action' => 'handleAction'
+    );
 
-	protected $reportObject;
-	
-	public function init() {
-		parent::init();
+    /**
+     * Variable that describes which report we are currently viewing based on 
+     * the URL (gets set in init method).
+     *
+     * @var string
+     */
+    protected $reportClass;
 
-		//set the report we are currently viewing from the URL
-		$this->reportClass = (isset($this->urlParams['ReportClass']) && $this->urlParams['ReportClass'] !== 'index')
-			? $this->urlParams['ReportClass']
-			: null;
-		$allReports = SS_Report::get_reports();
-		$this->reportObject = (isset($allReports[$this->reportClass])) ? $allReports[$this->reportClass] : null;
+    protected $reportObject;
+    
+    public function init()
+    {
+        parent::init();
 
-		// Set custom options for TinyMCE specific to ReportAdmin
-		HtmlEditorConfig::get('cms')->setOption('content_css', project() . '/css/editor.css');
-		HtmlEditorConfig::get('cms')->setOption('Lang', i18n::get_tinymce_lang());
+        //set the report we are currently viewing from the URL
+        $this->reportClass = (isset($this->urlParams['ReportClass']) && $this->urlParams['ReportClass'] !== 'index')
+            ? $this->urlParams['ReportClass']
+            : null;
+        $allReports = SS_Report::get_reports();
+        $this->reportObject = (isset($allReports[$this->reportClass])) ? $allReports[$this->reportClass] : null;
 
-		// Always block the HtmlEditorField.js otherwise it will be sent with an ajax request
-		Requirements::block(FRAMEWORK_DIR . '/javascript/HtmlEditorField.js');
-		Requirements::javascript(REPORTS_DIR . '/javascript/ReportAdmin.js');
-	}
+        // Set custom options for TinyMCE specific to ReportAdmin
+        HtmlEditorConfig::get('cms')->setOption('content_css', project() . '/css/editor.css');
+        HtmlEditorConfig::get('cms')->setOption('Lang', i18n::get_tinymce_lang());
 
-	/**
-	 * Does the parent permission checks, but also
-	 * makes sure that instantiatable subclasses of
-	 * {@link Report} exist. By default, the CMS doesn't
-	 * include any Reports, so there's no point in showing
-	 *
-	 * @param Member $member
-	 * @return boolean
-	 */
-	public function canView($member = null) {
-		if(!$member && $member !== FALSE) $member = Member::currentUser();
+        // Always block the HtmlEditorField.js otherwise it will be sent with an ajax request
+        Requirements::block(FRAMEWORK_DIR . '/javascript/HtmlEditorField.js');
+        Requirements::javascript(REPORTS_DIR . '/javascript/ReportAdmin.js');
+    }
 
-		if(!parent::canView($member)) return false;
+    /**
+     * Does the parent permission checks, but also
+     * makes sure that instantiatable subclasses of
+     * {@link Report} exist. By default, the CMS doesn't
+     * include any Reports, so there's no point in showing
+     *
+     * @param Member $member
+     * @return boolean
+     */
+    public function canView($member = null)
+    {
+        if (!$member && $member !== false) {
+            $member = Member::currentUser();
+        }
 
-		$hasViewableSubclasses = false;
-		foreach($this->Reports() as $report) {
-			if($report->canView($member)) return true;
-		}
+        if (!parent::canView($member)) {
+            return false;
+        }
 
-		return false;
-	}
+        $hasViewableSubclasses = false;
+        foreach ($this->Reports() as $report) {
+            if ($report->canView($member)) {
+                return true;
+            }
+        }
 
-	/**
-	 * Return a SS_List of SS_Report subclasses
-	 * that are available for use.
-	 *
-	 * @return SS_List
-	 */
-	public function Reports() {
- 		$output = new ArrayList();
-		foreach(SS_Report::get_reports() as $report) {
-			if($report->canView()) $output->push($report);
-		}
-		return $output;
-	}
+        return false;
+    }
 
-	/**
-	 * Determine if we have reports and need
-	 * to display the "Reports" main menu item
-	 * in the CMS.
-	 *
-	 * The test for an existance of a report
-	 * is done by checking for a subclass of
-	 * "SS_Report" that exists.
-	 *
-	 * @return boolean
-	 */
-	public static function has_reports() {
-		return sizeof(SS_Report::get_reports()) > 0;
-	}
+    /**
+     * Return a SS_List of SS_Report subclasses
+     * that are available for use.
+     *
+     * @return SS_List
+     */
+    public function Reports()
+    {
+        $output = new ArrayList();
+        foreach (SS_Report::get_reports() as $report) {
+            if ($report->canView()) {
+                $output->push($report);
+            }
+        }
+        return $output;
+    }
 
-	/**
-	 * Returns the Breadcrumbs for the ReportAdmin
-	 * @return ArrayList
-	 */
-	public function Breadcrumbs($unlinked = false) {
-		$items = parent::Breadcrumbs($unlinked);
-		
-		// The root element should explicitly point to the root node.
-		// Uses session state for current record otherwise.
-		$items[0]->Link = singleton('ReportAdmin')->Link();
+    /**
+     * Determine if we have reports and need
+     * to display the "Reports" main menu item
+     * in the CMS.
+     *
+     * The test for an existance of a report
+     * is done by checking for a subclass of
+     * "SS_Report" that exists.
+     *
+     * @return boolean
+     */
+    public static function has_reports()
+    {
+        return sizeof(SS_Report::get_reports()) > 0;
+    }
 
-		if ($this->reportObject) {
-			//build breadcrumb trail to the current report
-			$items->push(new ArrayData(array(
-					'Title' => $this->reportObject->title(),
-					'Link' => Controller::join_links($this->Link(), '?' . http_build_query(array('q' => $this->request->requestVar('q'))))
-				)));
-		}
+    /**
+     * Returns the Breadcrumbs for the ReportAdmin
+     * @return ArrayList
+     */
+    public function Breadcrumbs($unlinked = false)
+    {
+        $items = parent::Breadcrumbs($unlinked);
+        
+        // The root element should explicitly point to the root node.
+        // Uses session state for current record otherwise.
+        $items[0]->Link = singleton('ReportAdmin')->Link();
 
-		return $items;
-	}
+        if ($this->reportObject) {
+            //build breadcrumb trail to the current report
+            $items->push(new ArrayData(array(
+                    'Title' => $this->reportObject->title(),
+                    'Link' => Controller::join_links($this->Link(), '?' . http_build_query(array('q' => $this->request->requestVar('q'))))
+                )));
+        }
 
-	/**
-	 * Returns the link to the report admin section, or the specific report that is currently displayed
-	 * @return String
-	 */
-	public function Link($action = null) {
-		if ($this->reportObject) {
-			$link = $this->reportObject->getLink($action);
-		} else {
-			$link = self::join_links(parent::Link('index'), $action);
-		}
-		return $link;
-	}
+        return $items;
+    }
 
-	public function providePermissions() {
-		$title = _t("ReportAdmin.MENUTITLE", LeftAndMain::menu_title_for_class($this->class));
-		return array(
-			"CMS_ACCESS_ReportAdmin" => array(
-				'name' => _t('CMSMain.ACCESS', "Access to '{title}' section", array('title' => $title)),
-				'category' => _t('Permission.CMS_ACCESS_CATEGORY', 'CMS Access')
-			)
-		);
-	}
+    /**
+     * Returns the link to the report admin section, or the specific report that is currently displayed
+     * @return String
+     */
+    public function Link($action = null)
+    {
+        if ($this->reportObject) {
+            $link = $this->reportObject->getLink($action);
+        } else {
+            $link = self::join_links(parent::Link('index'), $action);
+        }
+        return $link;
+    }
 
-	public function getEditForm($id = null, $fields = null) {
-		$report = $this->reportObject;
-		if($report) {
-			$fields = $report->getCMSFields();
-		} else {
-			// List all reports
-			$fields = new FieldList();
-			$gridFieldConfig = GridFieldConfig::create()->addComponents(
-				new GridFieldToolbarHeader(),
-				new GridFieldSortableHeader(),
-				new GridFieldDataColumns(),
-				new GridFieldFooter()
-			);
-			$gridField = new GridField('Reports',false, $this->Reports(), $gridFieldConfig);
-			$columns = $gridField->getConfig()->getComponentByType('GridFieldDataColumns');
-			$columns->setDisplayFields(array(
-				'title' => _t('ReportAdmin.ReportTitle', 'Title'),
-			));
+    public function providePermissions()
+    {
+        $title = _t("ReportAdmin.MENUTITLE", LeftAndMain::menu_title_for_class($this->class));
+        return array(
+            "CMS_ACCESS_ReportAdmin" => array(
+                'name' => _t('CMSMain.ACCESS', "Access to '{title}' section", array('title' => $title)),
+                'category' => _t('Permission.CMS_ACCESS_CATEGORY', 'CMS Access')
+            )
+        );
+    }
 
-		        $columns->setFieldFormatting(array(
-		            'title' => '<a href=\"$Link\" class=\"cms-panel-link\">$value ($Count)</a>'
-		        ));			
-			$gridField->addExtraClass('all-reports-gridfield');
-			$fields->push($gridField);
-		}
+    public function getEditForm($id = null, $fields = null)
+    {
+        $report = $this->reportObject;
+        if ($report) {
+            $fields = $report->getCMSFields();
+        } else {
+            // List all reports
+            $fields = new FieldList();
+            $gridFieldConfig = GridFieldConfig::create()->addComponents(
+                new GridFieldToolbarHeader(),
+                new GridFieldSortableHeader(),
+                new GridFieldDataColumns(),
+                new GridFieldFooter()
+            );
+            $gridField = new GridField('Reports', false, $this->Reports(), $gridFieldConfig);
+            $columns = $gridField->getConfig()->getComponentByType('GridFieldDataColumns');
+            $columns->setDisplayFields(array(
+                'title' => _t('ReportAdmin.ReportTitle', 'Title'),
+            ));
 
-		$actions = new FieldList();
-		$form = new Form($this, "EditForm", $fields, $actions);
-		$form->addExtraClass('cms-edit-form cms-panel-padded center ' . $this->BaseCSSClasses());
-		$form->loadDataFrom($this->request->getVars());
+            $columns->setFieldFormatting(array(
+                    'title' => '<a href=\"$Link\" class=\"cms-panel-link\">$value ($Count)</a>'
+                ));
+            $gridField->addExtraClass('all-reports-gridfield');
+            $fields->push($gridField);
+        }
 
-		$this->extend('updateEditForm', $form);
+        $actions = new FieldList();
+        $form = new Form($this, "EditForm", $fields, $actions);
+        $form->addExtraClass('cms-edit-form cms-panel-padded center ' . $this->BaseCSSClasses());
+        $form->loadDataFrom($this->request->getVars());
 
-		return $form;
-	}
+        $this->extend('updateEditForm', $form);
+
+        return $form;
+    }
 }
